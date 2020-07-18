775149e1843a916f18917133dbf552ddaeae7f46
Rustam <rustamergashev.sp@gmail.com>
2020-07-18 22:11:42 +0300

    Секция Услуги готова: добавлены услуги. Функционал формы не реализована.

--- index.php
+++ index.php
@@ -2,8 +2,92 @@
   $APPLICATION->SetTitle("Новости банка");
 ?>
 
+  <div class="contentline" id="wehelp">
+    <div class="container">
+      <div class="headline">
+        <div class="h2">
+          <? $APPLICATION->IncludeComponent(
+            "bitrix:main.include",
+            ".default",
+            array(
+              "AREA_FILE_SHOW" => "file",
+              "AREA_FILE_SUFFIX" => "inc",
+              "EDIT_TEMPLATE" => "",
+              "COMPONENT_TEMPLATE" => ".default",
+              "PATH" => "/include/services-title.php"
+            ),
+            false
+          ); ?>
+        </div>
+      </div>
+      <? $APPLICATION->IncludeComponent(
+        "bitrix:news.list",
+        "services_template",
+        array(
+          "ACTIVE_DATE_FORMAT" => "d.m.Y",
+          "ADD_SECTIONS_CHAIN" => "Y",
+          "AJAX_MODE" => "N",
+          "AJAX_OPTION_ADDITIONAL" => "",
+          "AJAX_OPTION_HISTORY" => "N",
+          "AJAX_OPTION_JUMP" => "N",
+          "AJAX_OPTION_STYLE" => "Y",
+          "CACHE_FILTER" => "N",
+          "CACHE_GROUPS" => "Y",
+          "CACHE_TIME" => "36000000",
+          "CACHE_TYPE" => "A",
+          "CHECK_DATES" => "Y",
+          "DETAIL_URL" => "",
+          "DISPLAY_BOTTOM_PAGER" => "N",
+          "DISPLAY_DATE" => "Y",
+          "DISPLAY_NAME" => "Y",
+          "DISPLAY_PICTURE" => "Y",
+          "DISPLAY_PREVIEW_TEXT" => "Y",
+          "DISPLAY_TOP_PAGER" => "N",
+          "FIELD_CODE" => array(
+            0 => "NAME",
+            1 => "",
+          ),
+          "FILTER_NAME" => "",
+          "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+          "IBLOCK_ID" => "4",
+          "IBLOCK_TYPE" => "services",
+          "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+          "INCLUDE_SUBSECTIONS" => "Y",
+          "MESSAGE_404" => "",
+          "NEWS_COUNT" => "20",
+          "PAGER_BASE_LINK_ENABLE" => "N",
+          "PAGER_DESC_NUMBERING" => "N",
+          "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+          "PAGER_SHOW_ALL" => "N",
+          "PAGER_SHOW_ALWAYS" => "N",
+          "PAGER_TEMPLATE" => ".default",
+          "PAGER_TITLE" => "Услуги",
+          "PARENT_SECTION" => "",
+          "PARENT_SECTION_CODE" => "",
+          "PREVIEW_TRUNCATE_LEN" => "",
+          "PROPERTY_CODE" => array(
+            0 => "SERVICE",
+            1 => "",
+          ),
+          "SET_BROWSER_TITLE" => "Y",
+          "SET_LAST_MODIFIED" => "N",
+          "SET_META_DESCRIPTION" => "Y",
+          "SET_META_KEYWORDS" => "Y",
+          "SET_STATUS_404" => "N",
+          "SET_TITLE" => "Y",
+          "SHOW_404" => "N",
+          "SORT_BY1" => "ACTIVE_FROM",
+          "SORT_BY2" => "SORT",
+          "SORT_ORDER1" => "ASC",
+          "SORT_ORDER2" => "ASC",
+          "STRICT_SECTION_CHECK" => "N",
+          "COMPONENT_TEMPLATE" => "services_template"
+        )
+      ); ?>
 
-<?php // Отзывы ?>
+    </div>
+  </div>
+
   <section id="reviews">
     <div class="container">
       <div class="headline">
@@ -91,7 +175,6 @@
     </div>
   </section>
 
-<?php // Наши партнеры ?>
   <section id="partners">
     <div class="container">
       <div class="headline">
@@ -105,8 +188,7 @@
               "EDIT_TEMPLATE" => "",
               "COMPONENT_TEMPLATE" => ".default",
               "PATH" => "/include/partners-title.php"
-            ),
-            false
+            )
           ); ?>
         </h2>
       </div>
@@ -171,12 +253,12 @@
           "SORT_ORDER1" => "DESC",  // Направление для первой сортировки новостей
           "SORT_ORDER2" => "ASC",  // Направление для второй сортировки новостей
           "STRICT_SECTION_CHECK" => "N",  // Строгая проверка раздела для показа списка
-        ),
-        false
+        )
       ); ?>
 
 
     </div>
   </section>
+
 
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
