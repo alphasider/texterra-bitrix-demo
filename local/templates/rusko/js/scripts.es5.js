fb585293b0c17f46f682b31c4f6e367988fbb279
Rustam <rustamergashev.sp@gmail.com>
2020-07-20 06:14:01 +0300

    Форма обратной связи в футере реализована. Не реализовано: ввод номера телефона и валидация.

--- local/templates/rusko/js/scripts.es5.js
+++ local/templates/rusko/js/scripts.es5.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
     $('[data-pp="order"] [data-servicename]').val(""), $("#overlay, [data-pp]").fadeOut(), $("html, body").removeClass("fix");
   }), $("a.toankor").click(function (e) {
     var a = $(e.currentTarget).attr("href"),
-        t = $(a).offset();
+      t = $(a).offset();
     $(".navblock").removeClass("vis"), $("html, body").animate({
       scrollTop: t.top - 100
     }, 700);
@@ -46,7 +46,7 @@ $(document).ready(function () {
     $(this).attr("placeholder", $(this).data("orig-placeholder"));
   });
   var a = new URLSearchParams(window.location.search).get("slide"),
-      t = Array.from(document.querySelectorAll("#first .swiper-slide"));
+    t = Array.from(document.querySelectorAll("#first .swiper-slide"));
   a = a ? t.indexOf(document.querySelector("#" + a)) : 0, setTimeout(function () {
     new Swiper("#first .swiper-container", {
       slidesPerView: 1,
@@ -108,31 +108,31 @@ $(document).ready(function () {
 }), 0 == /MSIE \d|Trident.*rv:/.test(navigator.userAgent) && $(".lightyellow, .headline h2, .headline .h2").addClass("normal");
 
 var thanks = function thanks() {
-  $("[data-pp]").is(":visible") && $("[data-pp]").hide(), $('#overlay, [data-pp="thanks"]').fadeIn();
-},
-    scrollStatus = function scrollStatus() {
-  $(window).scrollTop() > 10 ? $("header .navblock, header .topline").addClass("scrolled") : $("header .navblock, header .topline").removeClass("scrolled");
-},
-    whyTab = function whyTab(e) {
-  var a = $(e);
-  $(e).siblings(".tabitem").removeClass("active"), $(e).addClass("active");
-  var t = a.next(".tabdesc").html();
-  a.parents(".tabholder").siblings(".tabcontent").find(".text").html(t);
-};
+    $("[data-pp]").is(":visible") && $("[data-pp]").hide(), $('#overlay, [data-pp="thanks"]').fadeIn();
+  },
+  scrollStatus = function scrollStatus() {
+    $(window).scrollTop() > 10 ? $("header .navblock, header .topline").addClass("scrolled") : $("header .navblock, header .topline").removeClass("scrolled");
+  },
+  whyTab = function whyTab(e) {
+    var a = $(e);
+    $(e).siblings(".tabitem").removeClass("active"), $(e).addClass("active");
+    var t = a.next(".tabdesc").html();
+    a.parents(".tabholder").siblings(".tabcontent").find(".text").html(t);
+  };
 
 $("#map").length && ymaps.ready(function () {
   var e = [55.809166, 37.611976];
   e = $(window).width() > 991 ? [55.809166, 37.601976] : [55.809166, 37.611976];
   var a = new ymaps.Map("map", {
-    center: e,
-    zoom: 15,
-    controls: []
-  }),
-      t = new ymaps.Placemark([55.809166, 37.611976], {
-    iconCaption: "РусКо",
-    balloonContent: "12751, г. Москва, 17-ый проезд Марьиной рощи, д. 4, корп. 1"
-  }, {
-    preset: "islands#blackPocketIcon"
-  });
+      center: e,
+      zoom: 15,
+      controls: []
+    }),
+    t = new ymaps.Placemark([55.809166, 37.611976], {
+      iconCaption: "РусКо",
+      balloonContent: "12751, г. Москва, 17-ый проезд Марьиной рощи, д. 4, корп. 1"
+    }, {
+      preset: "islands#blackPocketIcon"
+    });
   a.controls.add("zoomControl"), a.geoObjects.add(t), /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && a.behaviors.disable(["scrollZoom", "drag"]);
 });
